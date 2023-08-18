4260c8b3b17022fdb1ff4ef2804f19a0e67f1002
Vivekk9192 <vivekpatel10203@gmail.com>
2023-08-18 20:32:44 +0530

    update

--- src/about.jsx
+++
@@ -1,7 +0,0 @@
-import { NavLink } from "react-router-dom";
-export const About = (props) =>{
-    return <h1> About Component {props.info}<br/>
-        <NavLink to="/user">Go to User</NavLink><br/>
-        <NavLink to="/home">Go to Home</NavLink><br/>
-    </h1>
-};  
--- src/home.jsx
+++
@@ -1,7 +0,0 @@
-import { NavLink } from "react-router-dom";
-export const Home = () =>{
-    return <h1> Home Component <br/>
-        <NavLink to="/user">Go to User</NavLink><br/>
-        <NavLink to="/about">Go to About</NavLink><br/>
-    </h1>
-};  
